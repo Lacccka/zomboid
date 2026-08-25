ae3af22066b8505c71c292c19b144d23f9976b7c
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-25 21:01:39 +0200

    quest-framework: bump to 0.3.0 quest kernel

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
@@ -3,13 +3,14 @@ LCCQF = LCCQF or {}
 local Constants = {}
 
 Constants.MODULE = "LaccckaQuestFramework"
-Constants.VERSION = "0.2.9"
+Constants.VERSION = "0.3.0"
 Constants.LOG_PREFIX = "[LCCQF]"
 
 Constants.INTERACTION_RANGE = 3.25
 Constants.SERVER_INTERACTION_RANGE = 4.0
 Constants.RUNTIME_RESOLVE_PADDING = 1.0
 Constants.RUNTIME_RECONCILE_INTERVAL_MS = 250
+Constants.QUEST_UPDATE_INTERVAL_MS = 250
 Constants.REQUEST_COOLDOWN_MS = 150
 Constants.SESSION_TIMEOUT_MS = 15 * 60 * 1000
 Constants.COMMAND_HISTORY_TIMEOUT_MS = 30 * 60 * 1000
@@ -25,6 +26,10 @@ Constants.COMMAND = {
     RUNTIME_BINDINGS = "RuntimeBindings",
     RUNTIME_BINDING_UPSERT = "RuntimeBindingUpsert",
     RUNTIME_BINDING_REMOVE = "RuntimeBindingRemove",
+    REQUEST_QUESTS = "RequestQuests",
+    QUESTS = "Quests",
+    QUEST_UPSERT = "QuestUpsert",
+    QUEST_EVENT = "QuestEvent",
     DIALOGUE_STATE = "DialogueState",
     DIALOGUE_CLOSED = "DialogueClosed",
     STATUS = "Status",
@@ -33,6 +38,7 @@ Constants.COMMAND = {
 Constants.TEST_NPC_ID = "lccq_test_npc_01"
 Constants.TEST_NPC_BID = "e6b19154-2cf4-4ef9-9ad5-1ac8c85a0001"
 Constants.TEST_NPC_CID = "c2c9d2b8-7a21-4e66-a8d9-1cfa01caa001"
+Constants.TEST_QUEST_ID = "lccq_test_checkpoint"
 
 LCCQF.Constants = Constants
 
