eb0ea77b843f704f9ffae5eb20ebb00d4f3e6988
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-25 21:01:55 +0200

    quest-framework: connect Alexey dialogue to first quest

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/server/LCCQF/Content/LCCQFDialogueContent.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/server/LCCQF/Content/LCCQFDialogueContent.lua
@@ -1,5 +1,8 @@
+require "LCCQF/LCCQFConstants"
+
 LCCQF = LCCQF or {}
 
+local C = LCCQF.Constants
 local DialogueContent = LCCQF.DialogueContent or {}
 
 local dialogues = {
@@ -9,15 +12,60 @@ local dialogues = {
             start = {
                 textKey = "IGUI_LCCQF_Dialog_Start",
                 choices = {
-                    { id = "ask_work", textKey = "IGUI_LCCQF_Choice_AskWork", next = "work" },
+                    {
+                        id = "ask_work_available",
+                        textKey = "IGUI_LCCQF_Choice_AskWork",
+                        condition = { kind = "questState", questId = C.TEST_QUEST_ID, state = "available" },
+                        next = "work_offer",
+                    },
+                    {
+                        id = "ask_work_active",
+                        textKey = "IGUI_LCCQF_Choice_QuestInProgress",
+                        condition = { kind = "questState", questId = C.TEST_QUEST_ID, state = "active" },
+                        next = "work_active",
+                    },
+                    {
+                        id = "ask_work_completed",
+                        textKey = "IGUI_LCCQF_Choice_QuestCompletedTopic",
+                        condition = { kind = "questState", questId = C.TEST_QUEST_ID, state = "completed" },
+                        next = "work_completed",
+                    },
                     { id = "ask_identity", textKey = "IGUI_LCCQF_Choice_AskIdentity", next = "who" },
                     { id = "leave", textKey = "IGUI_LCCQF_Choice_Leave", close = true },
                 },
             },
-            work = {
-                textKey = "IGUI_LCCQF_Dialog_Work",
+            work_offer = {
+                textKey = "IGUI_LCCQF_Dialog_WorkOffer",
+                choices = {
+                    {
+                        id = "accept_checkpoint",
+                        textKey = "IGUI_LCCQF_Choice_AcceptQuest",
+                        condition = { kind = "questState", questId = C.TEST_QUEST_ID, state = "available" },
+                        action = { kind = "questAccept", questId = C.TEST_QUEST_ID },
+                        next = "work_accepted",
+                    },
+                    { id = "decline_checkpoint", textKey = "IGUI_LCCQF_Choice_DeclineQuest", next = "start" },
+                    { id = "leave", textKey = "IGUI_LCCQF_Choice_Leave", close = true },
+                },
+            },
+            work_accepted = {
+                textKey = "IGUI_LCCQF_Dialog_WorkAccepted",
                 choices = {
                     { id = "back", textKey = "IGUI_LCCQF_Choice_Understood", next = "start" },
+                    { id = "leave", textKey = "IGUI_LCCQF_Choice_Leave", close = true },
+                },
+            },
+            work_active = {
+                textKey = "IGUI_LCCQF_Dialog_WorkActive",
+                choices = {
+                    { id = "back", textKey = "IGUI_LCCQF_Choice_Back", next = "start" },
+                    { id = "leave", textKey = "IGUI_LCCQF_Choice_Leave", close = true },
+                },
+            },
+            work_completed = {
+                textKey = "IGUI_LCCQF_Dialog_WorkCompleted",
+                choices = {
+                    { id = "back", textKey = "IGUI_LCCQF_Choice_Back", next = "start" },
                     { id = "leave", textKey = "IGUI_LCCQF_Choice_Leave", close = true },
                 },
             },
