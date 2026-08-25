01130907c30a9fca9996375b7f0ab7b8f528b658
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-25 16:17:38 +0200

    Bump Quest Framework to 0.2.7

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
@@ -3,7 +3,7 @@ LCCQF = LCCQF or {}
 local Constants = {}
 
 Constants.MODULE = "LaccckaQuestFramework"
-Constants.VERSION = "0.2.6"
+Constants.VERSION = "0.2.7"
 Constants.LOG_PREFIX = "[LCCQF]"
 
 Constants.INTERACTION_RANGE = 3.25
