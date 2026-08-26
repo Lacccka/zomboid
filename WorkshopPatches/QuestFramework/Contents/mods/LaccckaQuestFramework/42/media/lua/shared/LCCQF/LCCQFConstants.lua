9a79f970633d71b2b438574fee833d81dffc45c3
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-26 09:53:53 +0200

    quest-framework: bump objective runtime to 0.3.3

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
@@ -3,7 +3,7 @@ LCCQF = LCCQF or {}
 local Constants = {}
 
 Constants.MODULE = "LaccckaQuestFramework"
-Constants.VERSION = "0.3.2"
+Constants.VERSION = "0.3.3"
 Constants.LOG_PREFIX = "[LCCQF]"
 
 Constants.INTERACTION_RANGE = 3.25
@@ -44,6 +44,7 @@ Constants.TEST_NPC_ID = "lccq_test_npc_01"
 Constants.TEST_NPC_BID = "e6b19154-2cf4-4ef9-9ad5-1ac8c85a0001"
 Constants.TEST_NPC_CID = "c2c9d2b8-7a21-4e66-a8d9-1cfa01caa001"
 Constants.TEST_QUEST_ID = "lccq_test_checkpoint"
+Constants.TEST_QUEST_2_ID = "lccq_test_supply_run"
 
 LCCQF.Constants = Constants
 
