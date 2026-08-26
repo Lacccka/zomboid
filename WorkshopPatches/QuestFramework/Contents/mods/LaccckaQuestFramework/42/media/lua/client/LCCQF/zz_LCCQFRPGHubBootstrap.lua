a6b108302eab4f4fa744d9228e196a4405b3277c
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-26 23:24:25 +0200

    quest-framework: bootstrap people journal and live portraits

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/client/LCCQF/zz_LCCQFRPGHubBootstrap.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/client/LCCQF/zz_LCCQFRPGHubBootstrap.lua
@@ -1,8 +1,12 @@
 require "LCCQF/Quest/LCCQFQuestClientState"
+require "LCCQF/Knowledge/LCCQFKnownPeopleClientState"
 require "LCCQF/Quest/LCCQFCharacterProjectionLifecycle"
+require "LCCQF/Knowledge/LCCQFCharacterKnowledgeLifecycle"
 require "LCCQF/Quest/LCCQFQuestMarkerService"
+require "LCCQF/Runtime/LCCQFBanditsClientPresentation"
 require "LCCQF/UI/LCCQFHub"
+require "LCCQF/UI/LCCQFRPGJournalPages"
 
-print("[LCCQF][CLIENT] RPG hub bootstrap loaded")
+print("[LCCQF][CLIENT] RPG hub bootstrap loaded people=true journal=true portrait=ISUI3DModel")
 
 return LCCQFHub
