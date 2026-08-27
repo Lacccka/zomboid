79416ae7027d62ce3ee05aa718f7e524aae7d979
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-27 23:56:10 +0200

    quest-framework: add faction knowledge protocol constants

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/shared/LCCQF/LCCQFConstants.lua
@@ -22,6 +22,7 @@ Constants.PERSISTENCE_SCHEMA_VERSION = 1
 Constants.QUEST_PERSISTENCE_SCHEMA_VERSION = 1
 Constants.KNOWLEDGE_PERSISTENCE_SCHEMA_VERSION = 1
 Constants.RELATIONSHIP_PERSISTENCE_SCHEMA_VERSION = 1
+Constants.FACTION_KNOWLEDGE_PERSISTENCE_SCHEMA_VERSION = 1
 Constants.CHARACTER_ID_MODDATA_KEY = "lccqCharacterId"
 
 Constants.COMMAND = {
@@ -40,6 +41,9 @@ Constants.COMMAND = {
     REQUEST_KNOWLEDGE = "RequestKnowledge",
     KNOWLEDGE = "Knowledge",
     KNOWN_PERSON_UPSERT = "KnownPersonUpsert",
+    REQUEST_FACTIONS = "RequestFactions",
+    FACTIONS = "Factions",
+    KNOWN_FACTION_UPSERT = "KnownFactionUpsert",
     DIALOGUE_STATE = "DialogueState",
     DIALOGUE_CLOSED = "DialogueClosed",
     STATUS = "Status",
@@ -48,6 +52,7 @@ Constants.COMMAND = {
 Constants.TEST_NPC_ID = "lccq_test_npc_01"
 Constants.TEST_NPC_BID = "e6b19154-2cf4-4ef9-9ad5-1ac8c85a0001"
 Constants.TEST_NPC_CID = "c2c9d2b8-7a21-4e66-a8d9-1cfa01caa001"
+Constants.TEST_FACTION_ID = "lccq_checkpoint_survivors"
 Constants.TEST_QUEST_ID = "lccq_test_checkpoint"
 Constants.TEST_QUEST_2_ID = "lccq_test_supply_run"
 
