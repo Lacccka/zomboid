9144b6eb67e8e9f8253d29dbdb2d458e33e1d08d
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-27 23:59:02 +0200

    quest-framework: bootstrap known factions UI

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/client/LCCQF/zz_LCCQFRPGHubBootstrap.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/client/LCCQF/zz_LCCQFRPGHubBootstrap.lua
@@ -1,13 +1,16 @@
 require "LCCQF/Quest/LCCQFQuestClientState"
 require "LCCQF/Knowledge/LCCQFKnownPeopleClientState"
+require "LCCQF/Faction/LCCQFKnownFactionsClientState"
 require "LCCQF/Quest/LCCQFCharacterProjectionLifecycle"
 require "LCCQF/Knowledge/LCCQFCharacterKnowledgeLifecycle"
+require "LCCQF/Faction/LCCQFCharacterFactionKnowledgeLifecycle"
 require "LCCQF/Quest/LCCQFQuestMarkerService"
 require "LCCQF/Runtime/LCCQFBanditsClientPresentation"
 require "LCCQF/UI/LCCQFHub"
 require "LCCQF/UI/LCCQFRPGJournalPages"
 require "LCCQF/Relationship/LCCQFKnownPeopleRelationshipPresentation"
+require "LCCQF/UI/LCCQFFactionPage"
 
-print("[LCCQF][CLIENT] RPG hub bootstrap loaded people=true journal=true portrait=ISUI3DModel relationships=true")
+print("[LCCQF][CLIENT] RPG hub bootstrap loaded people=true factions=true journal=true portrait=ISUI3DModel relationships=true")
 
 return LCCQFHub
