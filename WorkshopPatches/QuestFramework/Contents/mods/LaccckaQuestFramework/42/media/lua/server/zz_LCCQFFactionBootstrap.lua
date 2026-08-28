874b2ae90ea5afcc0ec7c96e98e0d84e355e2788
Lacccka <101393701+Lacccka@users.noreply.github.com>
2026-08-28 11:00:37 +0200

    quest-framework: bootstrap dry-run faction site allocator

--- WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/server/zz_LCCQFFactionBootstrap.lua
+++ WorkshopPatches/QuestFramework/Contents/mods/LaccckaQuestFramework/42/media/lua/server/zz_LCCQFFactionBootstrap.lua
@@ -1,6 +1,7 @@
 require "LCCQF/Faction/zz_LCCQFFactionDiscoveryBridge"
 require "LCCQF/Faction/zz_LCCQFFactionRelationshipBridge"
+require "LCCQF/FactionWorld/zz_LCCQFFactionSiteAllocator"
 
-print("[LCCQF][SERVER] faction bootstrap loaded discovery=true relationships=true")
+print("[LCCQF][SERVER] faction bootstrap loaded discovery=true relationships=true siteAllocator=dry-run")
 
 return true
